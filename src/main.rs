21b790c2c8fb3a4c111cb333ecfe5a020a9c8d40
Mikko Leppänen <mikko.leppanen@vaisala.com>
2023-11-25 19:00:48 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,7 @@ async fn handle_connection<T: Database>(mut stream: TcpStream, db: Arc<Mutex<T>>
                     GetValue::Error(_) => {
                         let mut db = db.lock().unwrap();
                         db.delete(&key);
-                        stream.write_all("-1\r\n".as_bytes()).unwrap();
+                        stream.write_all("$-1\r\n".as_bytes()).unwrap();
                     }
                     GetValue::Ok(value) => {
                         stream
