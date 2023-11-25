521a9ebd7b9bc3cd5641a6c3784bc31c37ca1fc8
Mikko Leppänen <mikko.leppanen@vaisala.com>
2023-11-25 12:34:53 +0200

    pass stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,59 @@
 use std::{
-    io::Write,
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
 };
 
 use anyhow::Result;
 
-fn handle_connections(stream: &mut TcpStream) -> Result<()> {
-    let res = "+PONG\r\n";
-    stream.write_all(res.as_bytes())?;
-    stream.flush()?;
+fn contains(haystack: &[u8], needle: &[u8]) -> bool {
+    haystack
+        .windows(needle.len())
+        .any(|window| window == needle)
+}
+
+fn parse_request(request: &[u8]) -> bool {
+    match request[0] {
+        b'*' => {
+            let size_end = request.iter().position(|&x| x == b'\r').unwrap();
+            let _array_size = std::str::from_utf8(&request[1..size_end])
+                .unwrap()
+                .parse::<usize>()
+                .unwrap();
+
+            contains(request, b"PING") || contains(request, b"ping")
+        }
+        _ => false,
+    }
+}
+
+fn read_from_stream(stream: &mut TcpStream) -> Option<Vec<u8>> {
+    let mut buf = [0; 512];
+    let size = stream.read(&mut buf);
+    match size {
+        Ok(size) => Some(buf[..size].to_vec()),
+        Err(_) => None,
+    }
+}
+
+fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+    while let Some(request) = read_from_stream(stream) {
+        if request.is_empty() {
+            break;
+        }
+        let is_ping = parse_request(&request);
+        match is_ping {
+            true => {
+                stream.write_all(b"+PONG\r\n").unwrap();
+            }
+            false => {
+                stream.write_all(b"-ERR unknown command\r\n").unwrap();
+            }
+        }
+    }
     Ok(())
 }
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap_or_else(|e| {
         panic!("failed to bind to socket: {}", e);
     });
@@ -24,7 +62,7 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                handle_connections(&mut stream).unwrap_or_else(|e| {
+                handle_connection(&mut stream).unwrap_or_else(|e| {
                     println!("failed to handle connection: {}", e);
                 });
             }
