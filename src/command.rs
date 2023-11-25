0ad008c8f34c9a4eb29d3aba8ff4361ae30fb2ef
Mikko Leppänen <mikko.leppanen@vaisala.com>
2023-11-25 14:57:06 +0200

    refactor

---
+++ src/command.rs
@@ -0,0 +1,49 @@
+use crate::response::Value;
+
+pub enum Command {
+    Ping,
+    Echo,
+}
+
+impl Command {
+    fn from_str(string: &str) -> Option<Command> {
+        match string {
+            "PING" => Some(Command::Ping),
+            "ECHO" => Some(Command::Echo),
+            _ => None,
+        }
+    }
+
+    pub fn process(&self, args: &[Value]) -> Option<String> {
+        match self {
+            Command::Ping => Some("PONG".to_string()),
+            Command::Echo => Some(
+                args.iter()
+                    .map(|arg| match arg {
+                        Value::String(string) => string.clone(),
+                        _ => "".to_string(),
+                    })
+                    .collect::<Vec<String>>()
+                    .join(" "),
+            ),
+        }
+    }
+
+    pub fn handle_command(value: &[Value]) -> Option<String> {
+        let command_name = &value[0];
+        let command_args = &value[1..];
+        match command_name {
+            Value::String(string) => match Command::from_str(string) {
+                Some(command) => command.process(command_args),
+                None => {
+                    eprintln!("unknown command: {}", string);
+                    None
+                }
+            },
+            _ => {
+                eprintln!("unexpected token: {:?}", command_name);
+                None
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+mod command;
 mod response;
-
+use command::Command;
 use response::{RespParser, Value};
 
 fn read_from_stream(stream: &mut TcpStream) -> Option<Vec<u8>> {
-    let mut buf = [0; 512];
+    let mut buf = [0; 1024];
     let size = stream.read(&mut buf);
     match size {
         Ok(size) => Some(buf[..size].to_vec()),
@@ -31,39 +32,12 @@ fn parse(data: &[u8]) -> Option<Value> {
     value
 }
 
-fn process_command(command: Vec<Value>) -> Option<String> {
-    let command_name = &command[0];
-    let command_args = &command[1..];
-    match command_name {
-        Value::String(string) => match string.to_lowercase().as_str() {
-            "ping" => Some("PONG".to_string()),
-            "echo" => command_args
-                .iter()
-                .map(|arg| match arg {
-                    Value::String(string) => string.clone(),
-                    _ => "".to_string(),
-                })
-                .collect::<Vec<String>>()
-                .join(" ")
-                .into(),
-            _ => {
-                eprintln!("unknown command: {}", string);
-                None
-            }
-        },
-        _ => {
-            eprintln!("unexpected token: {:?}", command_name);
-            None
-        }
-    }
-}
-
 fn process_request(request: &[u8]) -> Option<String> {
     let value = parse(request);
     match value {
-        Some(Value::Array(array)) => process_command(array),
+        Some(Value::Array(array)) => Command::handle_command(&array),
         _ => {
-            println!("unable to parse request");
+            eprintln!("unable to parse request");
             None
         }
     }
@@ -81,9 +55,9 @@ async fn handle_connection(mut stream: TcpStream) -> Result<()> {
                     .write_all(encode_response(response.as_bytes()).as_slice())
                     .unwrap();
             }
-            None => {
-                stream.write_all(b"-ERR unknown command\r\n").unwrap();
-            }
+            None => stream
+                .write_all(b"-ERR unknown command\r\n")
+                .expect("could not write to stream"),
         }
     }
     Ok(())
